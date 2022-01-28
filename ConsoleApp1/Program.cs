4339ac143fcc87a5a7a391700e17afc47977aa81
Darshan Makhecha <65150757+darshan-makhecha@users.noreply.github.com>
2022-01-28 19:09:10 +0530

    New Program Added

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            EmployeeDBContext context = new EmployeeDBContext();
+            // here return only IT and HR department name 
+            IEnumerable<Department> departments = context.Departments.Where(x => x.Name == "IT" || x.Name == "HR");
+
+                foreach (Department dept in departments) 
+                {
+                    Console.WriteLine("Department = " + dept.Name);
+                    // emp check if emp male then print name with department name
+                    IEnumerable<Employee> employees = dept.Employees.Where(x => x.Gender == "m");   
+                    foreach (Employee employee in employees) 
+                    {
+                        Console.WriteLine("\t Employee Name = " + employee.FirstName + " " + employee.LastName);
+                     }
+                    Console.WriteLine();
+                }
+            Console.Read();
+        }
+    }
+}
---
+++ RestrictionOperation/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic; // for list add namespace
+using System.Linq;      // for linq query add namespace
+namespace RestrictionOperation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int> {  1, 2, 3, 4, 5, 6, 7, 8, 9 ,10};
+            // add predicate for understand LINQ  where structure
+            // add i am Func<int,bool> because x => x % 2 == 0 where first x is return int and those after => this sign given true or false so 
+            // 2 method 
+
+          //  Func<int, bool> predicate = x => x % 2 == 0;
+          // IEnumerable<int> evennumber = numbers.Where(predicate);*//*
+
+            // select use the index number print with value like  0 - 1 , 1-2 ......
+            var result = numbers.Select((num ,index) => new  { Number = num , Index = index});
+           
+
+            IEnumerable<int> evennumber = numbers.Where(x => x % 2 == 0);
+            // output will same but structure change 
+            // i m use private() so also this methos willl accepts 
+            // 3 method 
+            //  IEnumerable<int> evennumber = numbers.Where(num =>IsEven(num));
+            // select use so print index and value print data
+            Console.WriteLine("Using Select print index number and data which pass in list");
+            foreach (var item in result)
+            {
+                Console.WriteLine(item.Number + " - " + item.Index);
+            }
+            Console.WriteLine();
+            int data = numbers.Count();
+            Console.WriteLine("So, the element of this list is {0}", data);
+            Console.WriteLine("===============");
+            Console.WriteLine();
+            Console.WriteLine("in list, Even number is :");
+            Console.WriteLine();
+
+
+            foreach (int i in evennumber)
+            {
+                Console.WriteLine(i);
+            }
+           
+
+        }
+        /*      3 method 
+                private static bool IsEven(int number) {
+                    return number % 2 == 0;
+                }*/
+    }
+}
